c2a52a16ebbc050a2f32bb9104a4fa2614827a67
Aravind <y.aravind1997@gmail.com>
2021-04-16 23:23:58 -0400

    improved problem_35

--- Problem_35.java
+++ Problem_35.java
@@ -1,8 +1,8 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int len=nums.length;
+        //int len=nums.length;
         int j=0;
-        for(int i=0;i<len;i++) {
+        for(int i=0;i<nums.length;i++) {
             if (nums[i] == target) {
                 return i;
             }
@@ -10,14 +10,11 @@ class Solution {
         
         while(target>nums[j]){
                     
-                    if(j== len-1){
+                    if(j== nums.length-1){
                         return j+1;
                     }
                    j++;
                 }
-
-
-
-        return j;
+         return j;
     }
 }
