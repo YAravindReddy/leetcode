e5a1ef3b8678ce101cfbff5fe74d05a8c4d976f1
Aravind <y.aravind1997@gmail.com>
2021-04-19 18:40:03 -0400

    problem 226

--- Problem_226.java
+++ Problem_226.java
@@ -15,15 +15,14 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-       if(root==null){
-           return null;
-       }
-        else{
-            TreeNode right= invertTree(root.right);
-            TreeNode left=invertTree(root.left);
-            root.left=right;
-            root.right=left;
-            return root;
-        }
+       if (root == null) {
+      return null;
+    }
+    TreeNode left = root.left;
+    root.left = root.right;
+    root.right = left;
+    invertTree(root.left);
+    invertTree(root.right);
+    return root;
     }
 }
